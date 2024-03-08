d0ce0dae0363c2a6e3330351f949beea9cdd7d41
HassanAkbar <hassanakbar2@gmail.com>
2024-03-08 18:25:03 +0500

    removed files from dir for windows

--- spec/acceptance/db2yaml_spec.rb
+++ spec/acceptance/db2yaml_spec.rb
@@ -133,6 +133,8 @@ RSpec.describe "IEV" do
 
         expect(concept2).to eq(expected_concept2)
         expect(localized_concept2).to eq(expected_localized_concept2)
+
+        FileUtils.rm_rf(concepts_dir)
       end
     end
   end
