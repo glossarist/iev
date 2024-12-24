9abf7247c9dc049fa4d25b2106ebce68a00ec354
Nick Nicholas <opoudjis@gmail.com>
2024-12-24 23:06:48 +0200

    rspec

--- spec/acceptance/db2yaml_spec.rb
+++ spec/acceptance/db2yaml_spec.rb
@@ -10,20 +10,8 @@ RSpec.describe "IEV" do
   describe "db2yaml" do
     let(:expected_concept1) do
       {
-        "data" => {
-          "identifier" => "103-01-01",
-          "localized_concepts" => {
-            "ara" => "0144dead-8444-5d99-947b-4b9433bdcbb2",
-            "deu" => "64541958-58d4-5e9d-95bf-643cc0fb3e9c",
-            "eng" => "2de73b1c-05e7-56c1-bdf2-d13cd06f7a96",
-            "fra" => "517f9c27-2b32-555b-813b-ee271fc1e293",
-            "jpn" => "4fa3997e-5c0d-5ec3-996b-6df06ba53446",
-            "kor" => "66ef2513-c970-57e9-9429-21bc8e8a2d69",
-            "pol" => "c37a8546-4175-51f8-b051-9dd8ef3fc2f7",
-            "por" => "8e3daaee-b1c6-53ec-81d1-e70f0b4e24bb",
-            "zho" => "f1981caa-f191-52a4-a012-be6883f17508",
-          },
-        },
+        "data" => {"identifier" => "103-01-01", "localized_concepts" => {"ara" => "a36d1b2f-f3cd-5cf2-ade8-df02e5b87d0c", "deu" => "8ec20cf3-980d-59ba-87df-fabd0456516f", "eng" => "2257cbc6-142f-5c7b-aa0a-9aefb4eaad66", "fra" => "76f9d5e5-c7e7-5b44-b416-97ea4cab88ba", "jpn" => "33419587-0fe6-5bdb-8b56-64b844a7bdf8", "kor" => "6aaae1be-cfcb-56ad-b0d9-800665788576", "pol" => "35de3a28-5eb3-5aeb-98eb-68ebda314c05", "por" => "77fd4c13-8e92-5a7e-801d-1147829466ce", "zho" => "1cc72f48-8133-57ed-8361-4a24c1ec9d4a"}},
+        "id" => "a9e98ffa-5960-5cd8-8f1e-9d0939da6fc0",
       }
     end
 
@@ -71,12 +59,16 @@ RSpec.describe "IEV" do
           "review_decision_date" => "2017-07-01T00:00:00+00:00",
           "review_decision_event" => "published",
         },
+        "date_accepted" => "2017-07-01T00:00:00+00:00",
+        "id" => "2257cbc6-142f-5c7b-aa0a-9aefb4eaad66",
+        "status" => "valid",
       }
     end
 
     let(:expected_concept2) do
       {
-        "data" => {"identifier" => "103-01-02", "localized_concepts" => {"ara" => "ab3f3785-fbed-5173-b6af-ca6eb0dcb3e1", "deu" => "d9c63649-bec9-554f-b012-06e174acecdb", "eng" => "4843a9ad-4204-54db-804d-278538631d97", "fra" => "36ae6c8f-6b5a-57d3-9f9b-712b1a37359b", "jpn" => "c3d3dc36-68cf-5ba3-8483-3d94ebb7d26a", "kor" => "88c5626d-0cd9-5616-8d04-e1a379eddda6"}}
+        "data" => {"identifier" => "103-01-02", "localized_concepts" => {"ara" => "b6fc2694-8d48-5cbf-9049-64604bbe72dd", "deu" => "b48bc0ef-422c-515f-81ec-3763d53d6e89", "eng" => "aeb511ce-9de0-5b98-a12d-5b920bd2dcd9", "fra" => "ba52518d-3e93-5b52-b18d-f2de54f620c8", "jpn" => "e08370c9-b29d-5984-8284-2051b1799d5a", "kor" => "f3593ebc-5afc-578a-942f-af346b59fba3"}},
+        "id" => "13ef6c25-6083-55e0-bcd8-85c887cafc6f",
       }
     end
 
@@ -99,6 +91,8 @@ RSpec.describe "IEV" do
           "entry_status" => "valid",
           "review_decision_event" => "published",
         },
+        "id" => "f3593ebc-5afc-578a-942f-af346b59fba3",
+        "status" => "valid",
       }
     end
 
