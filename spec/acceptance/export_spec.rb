a579e6ca71a855f09ce2aef92b320cc4db4de226
Ronald Tse <ronald.tse@ribose.com>
2026-04-27 16:51:08 +0800

    feat: add unified 'export' CLI command with Iev::Exporter model
    Add Iev::Exporter as the OOP domain model for the export pipeline,
    auto-detecting input format (xlsx vs sqlite) from file extension.
    
    The 'export' command replaces xlsx2yaml/db2yaml with a single entry
    point. Old commands delegate to Exporter internally for backward
    compatibility.
    
    Usage:
      exe/iev export data.xlsx -o /output
      exe/iev export data.sqlite3 -o /output
    
    Programmatic:
      Iev::Exporter.new('data.xlsx', output_dir: '/output').export

---
+++ spec/acceptance/export_spec.rb
@@ -0,0 +1,147 @@
+# frozen_string_literal: true
+
+require "spec_helper"
+require "yaml"
+
+RSpec.describe "Iev" do
+  let(:sample_xlsx) { fixture_path("sample-file.xlsx") }
+  let(:sample_db) { fixture_path("sample-db.sqlite3") }
+
+  describe "export" do
+    let(:expected_concept1) do
+      {
+        "data" => {
+          "identifier" => "103-01-01",
+          "localized_concepts" => {
+            "ara" => "2facc2da-b31c-5865-a461-7b09beb50306",
+            "deu" => "b428bdb9-caae-56e2-843e-fc235e237caf",
+            "eng" => "fb47471b-2692-5075-b273-b3f671e9ca9f",
+            "fra" => "c390520c-2eab-540e-bf70-bfa68a5a5d71",
+            "jpn" => "c7120404-7fea-5433-aa45-a2f314935991",
+            "kor" => "a89b8a05-ca41-5fb7-b435-c9bec070cd6f",
+            "pol" => "18ab22c6-5924-520b-978d-9344491a38e9",
+            "por" => "3d56d72b-8c68-52f8-8965-ecfdef4d0ec8",
+            "zho" => "a2ba7d83-7768-5460-a7df-7d3cd5246d84",
+          },
+        },
+        "id" => "a9e98ffa-5960-5cd8-8f1e-9d0939da6fc0",
+      }
+    end
+
+    let(:expected_localized_concept1) do
+      {
+        "data" => {
+          "dates" => [
+            {
+              "date" => "2017-07-01T00:00:00+00:00",
+              "type" => "accepted",
+            },
+            {
+              "date" => "2017-07-01T00:00:00+00:00",
+              "type" => "amended",
+            },
+          ],
+          "definition" => [
+            {
+              "content" => "See {{IEV 102-01-10, IEV:102-01-10}}",
+            },
+          ],
+          "examples" => [],
+          "id" => "103-01-01",
+          "notes" => [],
+          "terms" => [
+            {
+              "type" => "expression",
+              "normative_status" => "preferred",
+              "designation" => "function",
+            },
+          ],
+          "related" => [
+            {
+              "type" => "supersedes",
+              "ref" => {
+                "source" => "IEV",
+                "id" => "103-01-01",
+                "version" => "2009-12",
+              },
+            },
+          ],
+          "language_code" => "eng",
+          "entry_status" => "valid",
+          "review_date" => "2017-07-01T00:00:00+00:00",
+          "review_decision_date" => "2017-07-01T00:00:00+00:00",
+          "review_decision_event" => "published",
+        },
+        "date_accepted" => "2017-07-01T00:00:00+00:00",
+        "id" => "fb47471b-2692-5075-b273-b3f671e9ca9f",
+      }
+    end
+
+    it "exports YAMLs from a SQLite file" do
+      Dir.mktmpdir("iev-test") do |dir|
+        command = %W[export #{sample_db} -o #{dir}]
+        Iev::Cli.start(command)
+
+        concepts_dir = File.join(dir, "concepts")
+        expect(concepts_dir).to(satisfy { |p| File.directory? p })
+        expect(Dir["#{concepts_dir}/concept/*.yaml"]).not_to be_empty
+
+        concept1 = YAML.load_file(File.join(concepts_dir, "concept",
+                                            "a9e98ffa-5960-5cd8-8f1e-9d0939da6fc0.yaml"))
+        localized = YAML.load_file(File.join(concepts_dir, "localized_concept",
+                                             "#{concept1['data']['localized_concepts']['eng']}.yaml"))
+
+        expect(concept1).to be_yaml_equivalent_to(expected_concept1)
+        expect(localized).to be_yaml_equivalent_to(expected_localized_concept1)
+
+        FileUtils.rm_rf(concepts_dir)
+      end
+    end
+
+    it "exports YAMLs from an Excel file" do
+      Dir.mktmpdir("iev-test") do |dir|
+        command = %W[export #{sample_xlsx} -o #{dir}]
+        silence_output_streams { Iev::Cli.start(command) }
+
+        concepts_dir = File.join(dir, "concepts")
+        expect(concepts_dir).to(satisfy { |p| File.directory? p })
+        expect(Dir["#{concepts_dir}/concept/*.yaml"]).not_to be_empty
+
+        concept1 = YAML.load_file(File.join(concepts_dir, "concept",
+                                            "a9e98ffa-5960-5cd8-8f1e-9d0939da6fc0.yaml"))
+        localized = YAML.load_file(File.join(concepts_dir, "localized_concept",
+                                             "#{concept1['data']['localized_concepts']['eng']}.yaml"))
+
+        expect(concept1).to be_yaml_equivalent_to(expected_concept1)
+        expect(localized).to be_yaml_equivalent_to(expected_localized_concept1)
+
+        FileUtils.rm_rf(concepts_dir)
+      end
+    end
+  end
+
+  describe "Iev::Exporter" do
+    it "raises ArgumentError for unsupported file format" do
+      Dir.mktmpdir("iev-test") do |dir|
+        bad_file = File.join(dir, "data.csv")
+        File.write(bad_file, "not ieved data")
+        expect { Iev::Exporter.new(bad_file) }
+          .to raise_error(ArgumentError, /Unsupported format/)
+      end
+    end
+
+    it "raises ArgumentError for missing file" do
+      expect { Iev::Exporter.new("nonexistent.xlsx") }
+        .to raise_error(ArgumentError, /Input file not found/)
+    end
+
+    it "exports programmatically from a SQLite file" do
+      Dir.mktmpdir("iev-test") do |dir|
+        collection = Iev::Exporter.new(sample_db, output_dir: dir).export
+
+        expect(collection).to be_a(Glossarist::ManagedConceptCollection)
+        expect(Dir[File.join(dir, "concepts", "concept", "*.yaml")]).not_to be_empty
+      end
+    end
+  end
+end
